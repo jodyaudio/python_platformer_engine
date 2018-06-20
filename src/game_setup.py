77559b0c4113d2b77a893ddb9054248433020cf8
Bailey <jbailey@COXINC.COM>
2018-06-20 19:50:05 -0400

    initialized mixer module

--- src/game_setup.py
+++ src/game_setup.py
@@ -6,7 +6,11 @@ import constants as c
 import splash_screen
 
 
-# Initialize pygame and all of its modules.
+# Initialize pygame and all of its modules. !!!mixer module must be set up specifically like this or it will have
+# a really long delay (must be initialized before pygame itself). I could not find out why this is, but it is the only
+# way it works for some reason.
+pygame.mixer.pre_init(44100, -16, 2, 4096)
+pygame.mixer.init()
 pygame.init()
 
 
