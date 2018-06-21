6eae3732cc0bd891730b48e96655fc46312d824c
Bailey <jbailey@COXINC.COM>
2018-06-21 12:18:15 -0400

    Created init method

--- src/select_menu.py
+++ src/select_menu.py
@@ -4,5 +4,16 @@ import ultracolors as color
 
 
 class SelectMenu(pygame.sprite.Sprite):
-    def __init__(self):
+    """Simple menu class for generating selection menus that have
+    multiple options that can be highlighted and selected."""
+    def __init__(self, options_list):
         super(SelectMenu, self).__init__()
+        self.options_list = options_list
+        self.min_selection = 0
+        self.max_selection = len(options_list - 1)
+        self.current_index = 0
+        self.default_color = color.GREY
+        self.highlight_color = color.PURPLE_2
+        self.font_size = 12
+        self.font = pygame.font.Font(None, self.font_size)
+
